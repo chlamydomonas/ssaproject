9bf4b6c3cbbb3aadd97093ddf5f727b8cd9b4b82
chlamydomonas <chlamydomonas.a@gmail.com>
2014-09-24 23:40:47 +0900

    add category list in tablet

--- SSA/src/com/sds/ssa/tablet/ItemListFragment.java
+++ SSA/src/com/sds/ssa/tablet/ItemListFragment.java
@@ -1,13 +1,30 @@
 package com.sds.ssa.tablet;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
-import android.os.Bundle;
 import android.app.ListFragment;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.sds.ssa.R;
+import com.sds.ssa.adapter.CategoryRowAdapter;
 import com.sds.ssa.tablet.dummy.DummyContent;
+import com.sds.ssa.util.AppParams;
+import com.sds.ssa.util.Utils;
+import com.sds.ssa.vo.Category;
 
 /**
  * A list fragment representing a list of Items. This fragment also supports
@@ -20,6 +37,8 @@ import com.sds.ssa.tablet.dummy.DummyContent;
  */
 public class ItemListFragment extends ListFragment {
 
+	List<Category> categoryList;
+	
 	/**
 	 * The serialization (saved instance state) Bundle key representing the
 	 * activated item position. Only used on tablets.
@@ -71,9 +90,25 @@ public class ItemListFragment extends ListFragment {
 		super.onCreate(savedInstanceState);
 
 		// TODO: replace with a real list adapter.
-		setListAdapter(new ArrayAdapter<DummyContent.DummyItem>(getActivity(),
+		/*setListAdapter(new ArrayAdapter<DummyContent.DummyItem>(getActivity(),
 				android.R.layout.simple_list_item_activated_1,
-				android.R.id.text1, DummyContent.ITEMS));
+				android.R.id.text1, DummyContent.ITEMS));*/
+		
+		
+		categoryList = new ArrayList<Category>();
+
+		if (Utils.isNetworkAvailable(getActivity())) {
+			Locale systemLocale = getResources().getConfiguration().locale;
+			String systemLanguage = systemLocale.getLanguage();
+			String categoryLink = this.getString(R.string.server_address) + this.getString(R.string.category_link);
+			
+			if(systemLanguage.equals("en")){
+				categoryLink = categoryLink + "_en";
+			}
+			new MyTask().execute(categoryLink);
+		} else {
+			showToast("No Network Connection!!!");
+		}
 	}
 
 	@Override
@@ -86,6 +121,71 @@ public class ItemListFragment extends ListFragment {
 			setActivatedPosition(savedInstanceState
 					.getInt(STATE_ACTIVATED_POSITION));
 		}
+		
+		//onListItemClick(listView, view, position, id);
+	}
+
+	
+	class MyTask extends AsyncTask<String, Void, String> {
+		ProgressDialog pDialog;
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+
+			pDialog = new ProgressDialog(getActivity());
+			pDialog.setMessage("Loading...");
+			pDialog.setCancelable(false);
+			pDialog.show();
+		}
+
+		@Override
+		protected String doInBackground(String... params) {
+			return Utils.getJSONString(params[0]);
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+
+			if (null != pDialog && pDialog.isShowing()) {
+				pDialog.dismiss();
+			}
+
+			if (null == result || result.length() == 0) {
+				showToast("No data found from web!!!");
+				getActivity().finish();
+			} else {
+
+				try {					
+					JSONObject mainJson = new JSONObject(result);
+					JSONArray categoryArray = mainJson.getJSONArray(AppParams.CATEGORY_ARRAY_NAME);
+
+					for (int i = 0; i < categoryArray.length(); i++) {
+						JSONObject categoryObj = categoryArray.getJSONObject(i);
+
+						Category category = new Category();
+						category.setId(categoryObj.getString(AppParams.CATEGORY_ID));
+						category.setName(categoryObj.getString(AppParams.CATEGORY_NAME));
+						
+						categoryList.add(category);
+					}
+
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				setAdapterToListview();
+			}
+		}
+	}
+	
+	public void setAdapterToListview() {
+		setListAdapter(new CategoryRowAdapter(getActivity(), R.layout.category_row, categoryList));
+	}
+	
+	public void showToast(String msg) {
+		Toast.makeText(getActivity(), msg, Toast.LENGTH_LONG).show();
+		
 	}
 
 	@Override
